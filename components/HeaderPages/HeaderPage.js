56d5121892e947026500a00956aa8ce9d6cb505d
hiagoptk <hiagoptk@gmail.com>
2021-11-01 00:47:11 -0300

    reimplementando sideNav porem com o useState()

--- components/HeaderPages/HeaderPage.js
+++ components/HeaderPages/HeaderPage.js
@@ -1,19 +1,43 @@
 import {FaSun, FaBars, FaMoon, FaCode, FaPaintBrush, FaHandSparkles, FaJoint, FaHome, FaBrush, FaStar} from 'react-icons/fa'
 import {CgCardSpades} from 'react-icons/cg'
 import Link from 'next/link'
-import SideMenu from '../SideMenu/SideMenu';
+
 import styles from '../../styles/HeaderPage.module.css'
+import { useState } from 'react';
 
 
 
 const HeaderPage = () => {
+
+    const [sideMenu, setSideMenu] = useState(true);
+
+    const handleHideClick = () => {
+        setSideMenu(false);
+    };
+
+    const handleShowClick = () => {
+        setSideMenu(true);
+    }
+
     return (
         <navbar className={styles.navBlack}>
             <nav className={styles.menu}>
                 <Link href="/" passHref><h3 className={styles.title}>Hiago <FaSun className={styles.sun}/> Patrick</h3></Link>
-                <button id="menuBtn" className={styles.show} ><FaBars/></button>
+                <button id="menuBtn" onClick={handleShowClick} className={styles.show} ><FaBars/></button>
             </nav>
-            <SideMenu id="aside"  />
+            
+            <div className={sideMenu ? styles.show : styles.hide }>
+                <div id="sideMenu" className={styles.asideMenu}>
+                    <button onClick={handleHideClick} className={styles.closebtn} >&times;</button>
+                    <p className={styles.itemMenu}><Link href="/" passHref> Home </Link> <FaHome /></p>
+                    <p className={styles.itemMenu}><Link href="/tarot"passHref> Tarot  </Link><CgCardSpades/></p>
+                    <p className={styles.itemMenu}><Link href="/" passHref>Artes </Link> <FaPaintBrush /> </p>
+                    <p className={styles.itemMenu}><Link href="/reiki"passHref>Reiki </Link> <FaHandSparkles /> </p>
+                    <p className={styles.itemMenu}><Link href="/bruxaria" passHref>Bruxaria </Link> <FaStar /> </p>
+                    <p className={styles.itemMenu}><Link href="/portifolio" passHref>Portifolio</Link> <FaCode /></p>
+                </div>
+            </div>
+             
         </navbar>
     );
 };
